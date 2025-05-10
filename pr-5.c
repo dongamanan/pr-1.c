00a415cb4e35952916d4a0f9799d0564b41076eb
dongamanan <dongadaya2009@gmail.com>
2025-05-10 10:47:00 +0530

    Add files via upload

---
+++ pr-5.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	float a,b,c,d;
+	
+	printf("enter value of a:");
+	scanf("%f",&a);
+	
+	printf("enter value of b :");
+	scanf("%f",&a);
+	
+	c=(a+b)-180;
+	
+	
+	printf("c is :%f",c);
+	
+	
+	
+	
+	
+}
+
+
+s
